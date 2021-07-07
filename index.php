e71ad9c4366a197c52150a01d232ae53e55de9cd
alan_ms <shawwhip@gmail.com>
2021-07-07 01:00:09 -0300

    setting curl commands

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
-    echo 'test';
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_HTTPHEADER => array("Content-Type: application/json"),
+    CURLOPT_URL => "https://api.reddit.com/r/artificial/hot",
+    CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+    CURLOPT_RETURNTRANSFER => 1
+]);
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+echo $response;
 
 ?>
